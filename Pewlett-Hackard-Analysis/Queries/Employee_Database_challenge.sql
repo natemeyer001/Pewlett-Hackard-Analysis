b2b8129292f0607ce26736eb32c2a30a02814980
natemeyer001 <30487641+natemeyer001@users.noreply.github.com>
2021-09-22 14:15:09 -0500

    Add files via upload

---
+++ Pewlett-Hackard-Analysis/Queries/Employee_Database_challenge.sql
@@ -0,0 +1,37 @@
+-- Create new table with requested fields
+SELECT e.emp_no, e.first_name, e.last_name,
+	ti.title, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti 
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name, last_name, title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
+
+-- Number of employees by (most recent) job title 
+-- who are about to retire
+SELECT COUNT(title) as t_count, title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY t_count DESC;
+
+-- Mentorship Eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name, e.last_name, e.birth_date,
+	de.from_date, de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti ON (ti.emp_no = e.emp_no)
+WHERE (de.to_date = ('9999-01-01'))
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
